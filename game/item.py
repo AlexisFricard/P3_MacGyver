263a6ac4cc70478bfdeebf3c63be08e8baed1791
Alexis Fricard <a.fricardpro@gmail.com>
2020-10-03 16:12:16 +0200

    Modification de item.py, utilisation de Pylint

--- game/item.py
+++ game/item.py
@@ -1,14 +1,19 @@
+"""
+Item's Module - To create an object
+"""
+
 from game.constants import TILE_SIZE
 
 class Item:
-	#Création d'un objet item
-	def __init__ (self, position):
-		# pos_x = first attribute of list, etc..
-		self.pos_x  = position[0]
-		self.pos_y  = position[1]
-		# to show or delete item if macgyver have allready it
-		self.visible = True
+    """ Creat an item with position """
+    def __init__ (self, position):
+        # pos_x = first attribute of list, etc..
+        self.pos_x  = position[0]
+        self.pos_y  = position[1]
+        # to show or delete item if macgyver have allready it
+        self.visible = True
 
-	@property
-	def sprite_position(self):
-		return (self.pos_x * TILE_SIZE, self.pos_y * TILE_SIZE)
+    @property
+    def sprite_position(self):
+        """ Gives position's dimensions of the sprite """
+        return (self.pos_x * TILE_SIZE, self.pos_y * TILE_SIZE)
