168150a5a3a41e85de5baa86512d6153bda63ac0
Alexis Fricard <a.fricardpro@gmail.com>
2020-10-03 16:10:04 +0200

    Modification de level.py, utilisation de Pylint

--- game/level.py
+++ game/level.py
@@ -1,35 +1,40 @@
+"""
+Level module - Initializes the items and to a function if the path is free
+"""
+
 from random import randint
-import game.item
+import game.item as itm
 from game.constants import TILE_WIDTH, TILE_HEIGHT
 
 class Level:
+    """ Get item and is position, and if character are autorized to pass """
+    def __init__(self, maze_map, items):
+        self.maze_map = maze_map
+        self.items = {}
+        for item in items:
+            self.items[item] = itm.Item(self.random_item_position())
 
-	def __init__(self, maze_map, items):
-		# load maze to modify it top put object/item
-		self.maze_map = maze_map
-		self.items = {}
-		# for each args (here : item1 and item2) in items list
-		for item in items:
-			# create an object who have a list of random position
-			self.items[item] = game.item.Item(self.random_item_position())
+    def random_item_position(self):
+        """ Give a random position to the item """
+        pos_x = 0
+        pos_y = 0
+        while self.maze_map[pos_y][pos_x] != " ":
+            pos_x = randint(0, TILE_WIDTH - 1 )
+            pos_y = randint(0, TILE_HEIGHT - 1)
+        self.maze_map[pos_y][pos_x] = "-"
+        return (pos_x, pos_y)
 
-	def random_item_position(self):
-		pos_x = 0
-		pos_y = 0
-		while self.maze_map[pos_y][pos_x] != " ":
-			pos_x = randint(0, TILE_WIDTH - 1 )
-			pos_y = randint(0, TILE_HEIGHT - 1)
-		self.maze_map[pos_y][pos_x] = "-"
-		return (pos_x, pos_y)
-
-	def autorized_path(self, pos_x, pos_y, direction):
-		#if direction is not a wall, a ang_wall, and the limit of window
-		# A REVOIR CAR PAS TOUT COMPRIS
-		if direction == 'RIGHT' and pos_x < (TILE_WIDTH - 1) and self.maze_map[pos_y][pos_x + 1] != "W" and self.maze_map[pos_y][pos_x + 1] != "A":
-			return True
-		elif direction == 'LEFT' and pos_x > 0 and self.maze_map[pos_y][pos_x - 1] != "W" and self.maze_map[pos_y][pos_x - 1] != "A":
-			return True
-		elif direction == 'UP' and pos_y > 0 and self.maze_map[pos_y - 1][pos_x] != "W" and self.maze_map[pos_y - 1][pos_x] != "A":
-			return True
-		elif direction == 'DOWN' and pos_y < (TILE_HEIGHT - 1) and self.maze_map[pos_y + 1][pos_x] != "W" and self.maze_map[pos_y + 1][pos_x] != "A":
-			return True
+    def autorized_path(self, pos_x, pos_y, direction):
+        """ Passage check """
+        return (direction == 'RIGHT' and pos_x < (TILE_WIDTH - 1) and
+        self.maze_map[pos_y][pos_x + 1] != "W" and
+        self.maze_map[pos_y][pos_x + 1] != "A" or
+        direction == 'LEFT' and pos_x > 0 and
+        self.maze_map[pos_y][pos_x - 1] != "W" and
+        self.maze_map[pos_y][pos_x - 1] != "A" or
+        direction == 'UP' and pos_y > 0 and
+        self.maze_map[pos_y - 1][pos_x] != "W" and
+        self.maze_map[pos_y - 1][pos_x] != "A" or
+        direction == 'DOWN' and pos_y < (TILE_HEIGHT - 1) and
+        self.maze_map[pos_y + 1][pos_x] != "W" and
+        self.maze_map[pos_y + 1][pos_x] != "A")
